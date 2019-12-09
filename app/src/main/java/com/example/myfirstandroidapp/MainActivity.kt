a8c58840e384d82b222ad15c0ca71c4cfce11b50
SwapnanilDhol <41509261+SwapnanilDhol@users.noreply.github.com>
2019-12-09 15:38:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myfirstandroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+            progressTextView.text = p1.toString()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+        buttonOnClick()
+
+
+    }
+
+    fun buttonOnClick()
+    {
+        resetButton.setOnClickListener{ v ->
+            seekBar.progress = 0
+
+        }
+    }
+
+}
